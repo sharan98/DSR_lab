192b1590e3087d2faa2f7fff0fcf1cb54b90c67a
sharan98 <rudreshsharan@gmail.com>
2019-09-28 12:13:15 +0530

    Second bees dataframe

---
+++ Second/second_lab_bees.r
@@ -0,0 +1,8 @@
+buff_tail = c(10,1,37,5,12)
+garden_bee = c(8, 3, 9, 6, 4)
+red_tail = c(18, 9, 1, 2, 4)
+carder_bee = c(8, 27, 6, 32, 23)
+honey_bee = c(12, 13, 16, 9, 10)
+plant_names = c("Thistle", "Vipers", "Golden rain", "Yellow alfalfa", "blackberry")
+
+df = data.frame(buff_tail, garden_bee, red_tail, carder_bee, honey_bee, row.names = plant_names)
